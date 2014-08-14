eb90c8d9b0effd0c4bf0a25a9fa884c6c6d93b8c
Devon Govett <devongovett@gmail.com>
2014-08-13 20:56:53 -0700

    Add windows support for font substitution

--- src/FontManagerWindows.cc
+++ src/FontManagerWindows.cc
@@ -204,3 +204,212 @@ Handle<Value> findFont(FontDescriptor *desc) {
 
   return Null();
 }
+
+Handle<Value> findFonts(FontDescriptor *desc) {
+  return Null();
+}
+
+// custom text renderer used to determine the fallback font for a given char
+class FontFallbackRenderer : public IDWriteTextRenderer {
+public:
+  IDWriteFontCollection *systemFonts;
+  IDWriteFont *font;
+  unsigned long refCount;
+
+  FontFallbackRenderer(IDWriteFontCollection *collection) {
+    refCount = 0;
+    collection->AddRef();
+    systemFonts = collection;
+    font = NULL;
+  }
+
+  ~FontFallbackRenderer() {
+    if (systemFonts)
+      systemFonts->Release();
+
+    if (font)
+      font->Release();
+  }
+
+  // IDWriteTextRenderer methods
+  IFACEMETHOD(DrawGlyphRun)(
+      void *clientDrawingContext,
+      FLOAT baselineOriginX,
+      FLOAT baselineOriginY,
+      DWRITE_MEASURING_MODE measuringMode,
+      DWRITE_GLYPH_RUN const *glyphRun,
+      DWRITE_GLYPH_RUN_DESCRIPTION const *glyphRunDescription,
+      IUnknown *clientDrawingEffect) {
+
+    // save the font that was actually rendered
+    return systemFonts->GetFontFromFontFace(glyphRun->fontFace, &font);
+  }
+
+  IFACEMETHOD(DrawUnderline)(
+      void *clientDrawingContext,
+      FLOAT baselineOriginX,
+      FLOAT baselineOriginY,
+      DWRITE_UNDERLINE const *underline,
+      IUnknown *clientDrawingEffect) {
+    return E_NOTIMPL;
+  }
+
+
+  IFACEMETHOD(DrawStrikethrough)(
+      void *clientDrawingContext,
+      FLOAT baselineOriginX,
+      FLOAT baselineOriginY,
+      DWRITE_STRIKETHROUGH const *strikethrough,
+      IUnknown *clientDrawingEffect) {
+    return E_NOTIMPL;
+  }
+
+
+  IFACEMETHOD(DrawInlineObject)(
+      void *clientDrawingContext,
+      FLOAT originX,
+      FLOAT originY,
+      IDWriteInlineObject *inlineObject,
+      BOOL isSideways,
+      BOOL isRightToLeft,
+      IUnknown *clientDrawingEffect) {
+    return E_NOTIMPL;
+  }
+
+  // IDWritePixelSnapping methods
+  IFACEMETHOD(IsPixelSnappingDisabled)(void *clientDrawingContext, BOOL *isDisabled) {
+    *isDisabled = FALSE;
+    return S_OK;
+  }
+
+  IFACEMETHOD(GetCurrentTransform)(void *clientDrawingContext, DWRITE_MATRIX *transform) {
+    const DWRITE_MATRIX ident = {1.0, 0.0, 0.0, 1.0, 0.0, 0.0};
+    *transform = ident;
+    return S_OK;
+  }
+
+  IFACEMETHOD(GetPixelsPerDip)(void *clientDrawingContext, FLOAT *pixelsPerDip) {
+    *pixelsPerDip = 1.0f;
+    return S_OK;
+  }
+
+  // IUnknown methods
+  IFACEMETHOD_(unsigned long, AddRef)() {
+    return InterlockedIncrement(&refCount);
+  }
+
+  IFACEMETHOD_(unsigned long,  Release)() {
+    unsigned long newCount = InterlockedDecrement(&refCount);
+    if (newCount == 0) {
+      delete this;
+      return 0;
+    }
+
+    return newCount;
+  }
+
+  IFACEMETHOD(QueryInterface)(IID const& riid, void **ppvObject) {
+    if (__uuidof(IDWriteTextRenderer) == riid) {
+      *ppvObject = this;
+    } else if (__uuidof(IDWritePixelSnapping) == riid) {
+      *ppvObject = this;
+    } else if (__uuidof(IUnknown) == riid) {
+      *ppvObject = this;
+    } else {
+      *ppvObject = nullptr;
+      return E_FAIL;
+    }
+
+    this->AddRef();
+    return S_OK;
+  }
+};
+
+Handle<Value> substituteFont(char *postscriptName, char *string) {
+  Handle<Value> res = Null();
+
+  IDWriteFactory *factory = NULL;
+  HR(DWriteCreateFactory(
+    DWRITE_FACTORY_TYPE_SHARED,
+    __uuidof(IDWriteFactory),
+    reinterpret_cast<IUnknown**>(&factory)
+  ));
+
+  // Get the system font collection.
+  IDWriteFontCollection *collection = NULL;
+  HR(factory->GetSystemFontCollection(&collection));
+
+  // find the font for the given postscript name
+  FontDescriptor *desc = new FontDescriptor();
+  desc->postscriptName = postscriptName;
+  IDWriteFont *font = findFontByPostscriptName(collection, desc);
+
+  if (font) {
+    // get the font family name
+    IDWriteFontFamily *family = NULL;
+    HR(font->GetFontFamily(&family));
+
+    IDWriteLocalizedStrings *names = NULL;
+    HR(family->GetFamilyNames(&names));
+
+    unsigned int length = 0;
+    HR(names->GetStringLength(0, &length));
+    WCHAR *familyName = new WCHAR[length + 1];
+    HR(names->GetString(0, familyName, length + 1));
+
+    // convert utf8 string for substitution to utf16
+    unsigned int strLen = MultiByteToWideChar(CP_UTF8, 0, string, -1, NULL, 0);
+    WCHAR *str = new WCHAR[strLen];
+    MultiByteToWideChar(CP_UTF8, 0, string, -1, str, strLen);
+
+    // create a text format
+    IDWriteTextFormat *format = NULL;
+    HR(factory->CreateTextFormat(
+      familyName,
+      collection,
+      font->GetWeight(),
+      font->GetStyle(),
+      font->GetStretch(),
+      12.0,
+      L"en-us",
+      &format
+    ));
+
+    // create a text layout for the substitution string
+    IDWriteTextLayout *layout = NULL;
+    HR(factory->CreateTextLayout(
+      str,
+      strLen - 1, // ignore null character
+      format,
+      100.0,
+      100.0,
+      &layout
+    ));
+
+    // render it using a custom renderer that saves the physical font being used
+    FontFallbackRenderer *renderer = new FontFallbackRenderer(collection);
+    HR(layout->Draw(NULL, renderer, 100.0, 100.0));
+
+    // if we found something, create a result object
+    if (renderer->font) {
+      res = resultFromFont(renderer->font);
+    }
+
+    // free all the things
+    delete renderer;
+    layout->Release();
+    format->Release();
+    delete str;
+    delete familyName;
+    names->Release();
+    family->Release();
+    font->Release();
+  }
+
+  desc->postscriptName = NULL;
+  delete desc;
+  collection->Release();
+  factory->Release();
+
+  return res;
+}
