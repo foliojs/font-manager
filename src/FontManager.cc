d095851f4997a3a18e6023932b4ee8df7b3de4f0
Devon Govett <devongovett@gmail.com>
2014-08-15 22:44:22 -0400

    Update FontManager to use ResultSet and FontManagerResult classes

--- src/FontManager.cc
+++ src/FontManager.cc
@@ -2,16 +2,60 @@
 #include <node.h>
 #include <v8.h>
 #include "FontDescriptor.h"
+#include "FontManagerResult.h"
 
 using namespace v8;
 
 // these functions are implemented by the platform
-Handle<Value> getAvailableFonts(const Arguments&);
-Handle<Value> findFonts(FontDescriptor *);
-Handle<Value> findFont(FontDescriptor *);
-Handle<Value> substituteFont(char *, char *);
+ResultSet *getAvailableFonts();
+ResultSet *findFonts(FontDescriptor *);
+FontManagerResult *findFont(FontDescriptor *);
+FontManagerResult *substituteFont(char *, char *);
 
-Handle<Value> findFontFn(const Arguments& args, Handle<Value> (*fn)(FontDescriptor *)) {
+// converts a ResultSet to a JavaScript array
+Local<Array> collectResults(ResultSet *results) {
+  Local<Array> res = Array::New(results->size());
+    
+  int i = 0;
+  for (ResultSet::iterator it = results->begin(); it != results->end(); it++) {
+    res->Set(i++, (*it)->toJSObject());
+  }
+  
+  delete results;
+  return res;
+}
+
+// converts a FontManagerResult to a JavaScript object
+Local<Object> wrapResult(FontManagerResult *result) {
+  Local<Object> res = result->toJSObject();
+  delete result;
+  return res;
+}
+
+Handle<Value> getAvailableFonts(const Arguments& args) {
+  HandleScope scope;
+  return scope.Close(collectResults(getAvailableFonts()));
+}
+
+Handle<Value> findFonts(const Arguments& args) {
+  HandleScope scope;
+  
+  if (args.Length() < 1) {
+    ThrowException(Exception::TypeError(String::New("Missing font descriptor")));
+    return scope.Close(Undefined());
+  }
+  
+  if (!args[0]->IsObject()) {
+    ThrowException(Exception::TypeError(String::New("Not a font descriptor")));
+    return scope.Close(Undefined());
+  }
+  
+  Local<Object> desc = Local<Object>::Cast(args[0]);
+  FontDescriptor *descriptor = new FontDescriptor(desc);
+  return scope.Close(collectResults(findFonts(descriptor)));
+}
+
+Handle<Value> findFont(const Arguments& args) {  
   HandleScope scope;
   
   if (args.Length() < 1) {
@@ -26,15 +70,7 @@ Handle<Value> findFontFn(const Arguments& args, Handle<Value> (*fn)(FontDescript
 
   Local<Object> desc = Local<Object>::Cast(args[0]);
   FontDescriptor *descriptor = new FontDescriptor(desc);
-  return scope.Close(fn(descriptor));
-}
-
-Handle<Value> findFonts(const Arguments& args) {
-  return findFontFn(args, findFonts);
-}
-
-Handle<Value> findFont(const Arguments& args) {
-  return findFontFn(args, findFont);
+  return scope.Close(wrapResult(findFont(descriptor)));
 }
 
 Handle<Value> substituteFont(const Arguments& args) {
@@ -58,14 +94,14 @@ Handle<Value> substituteFont(const Arguments& args) {
   v8::String::Utf8Value postscriptName(args[0]);
   v8::String::Utf8Value subtitutionString(args[1]);
   
-  return scope.Close(substituteFont(*postscriptName, *subtitutionString));
+  return scope.Close(wrapResult(substituteFont(*postscriptName, *subtitutionString)));
 }
 
 void init(Handle<Object> exports) {
-  exports->Set(String::NewSymbol("getAvailableFonts"), FunctionTemplate::New(getAvailableFonts)->GetFunction());
-  exports->Set(String::NewSymbol("findFonts"), FunctionTemplate::New(findFonts)->GetFunction());
-  exports->Set(String::NewSymbol("findFont"), FunctionTemplate::New(findFont)->GetFunction());
-  exports->Set(String::NewSymbol("substituteFont"), FunctionTemplate::New(substituteFont)->GetFunction());
+  NODE_SET_METHOD(exports, "getAvailableFonts", getAvailableFonts);
+  NODE_SET_METHOD(exports, "findFonts", findFonts);
+  NODE_SET_METHOD(exports, "findFont", findFont);
+  NODE_SET_METHOD(exports, "substituteFont", substituteFont);
 }
 
 NODE_MODULE(fontmanager, init)
