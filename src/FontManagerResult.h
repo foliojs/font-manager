e20591cf541b8d9aeff7313bb37fb9beb5513755
Devon Govett <devongovett@gmail.com>
2014-08-15 22:43:00 -0400

    Create FontManagerResult class

--- src/FontManagerResult.h
+++ src/FontManagerResult.h
@@ -1,26 +1,28 @@
 #ifndef FONT_MANAGER_RESULT_H
 #define FONT_MANAGER_RESULT_H
 #include <v8.h>
+#include <vector>
 
 using namespace v8;
-using namespace node;
 
-Local<Object> createResult(const char *path, const char *postscriptName) {
-  Local<Object> res = Object::New();
+class FontManagerResult {
+public:
+  FontManagerResult(const char *path, const char *postscriptName) {
+    this->path = path;
+    this->postscriptName = postscriptName;
+  }
+  
+  Local<Object> toJSObject() {
+    Local<Object> res = Object::New();
+    res->Set(String::NewSymbol("path"), String::New(path));
+    res->Set(String::NewSymbol("postscriptName"), String::New(postscriptName));
+    return res;
+  }
+  
+private:
+  const char *path;
+  const char *postscriptName;
+};
 
-  res->Set(String::NewSymbol("path"), String::New(path));
-  res->Set(String::NewSymbol("postscriptName"), String::New(postscriptName));
-
-  return res;
-}
-
-Local<Object> createResult(const uint16_t *path, const uint16_t *postscriptName) {
-  Local<Object> res = Object::New();
-
-  res->Set(String::NewSymbol("path"), String::New(path));
-  res->Set(String::NewSymbol("postscriptName"), String::New(postscriptName));
-
-  return res;
-}
 
 #endif
