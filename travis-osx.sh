bdfc92c7854c8c12cd572e6a60357819f013297a
Devon Govett <devongovett@gmail.com>
2014-08-22 23:28:30 -0400

    Run nvm outside shell script

--- travis-osx.sh
+++ travis-osx.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # install nvm since it doesn't appear to be on the OS X VMs
-curl https://raw.githubusercontent.com/creationix/nvm/v0.13.1/install.sh | bash
-source ~/.nvm/nvm.sh
+curl -s https://raw.githubusercontent.com/creationix/nvm/v0.13.1/install.sh | bash
