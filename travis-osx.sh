b1d40bd99afd175195b502e4262116f5dc591049
Devon Govett <devongovett@gmail.com>
2014-08-22 23:17:37 -0400

    New attempt

--- travis-osx.sh
+++ travis-osx.sh
@@ -2,4 +2,4 @@
 
 # install nvm since it doesn't appear to be on the OS X VMs
 curl https://raw.githubusercontent.com/creationix/nvm/v0.13.1/install.sh | bash
-. ~/.bash_profile
+source ~/.nvm/nvm.sh
