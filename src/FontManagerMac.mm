4be4d59f10fd4e622dd527af748e211e26cf460b
Tyler Wanek <tylerw@axosoft.com>
2018-09-14 13:56:21 -0700

    Don't "find" fonts that have missing attributes
    Co-authored-by: Jake Krammer <jakek@axosoft.com>

--- src/FontManagerMac.mm
+++ src/FontManagerMac.mm
@@ -33,23 +33,41 @@ static int convertWidth(float unit) {
   }
 }
 
-FontDescriptor *createFontDescriptor(CTFontDescriptorRef descriptor) {
+FontDescriptor *getMaybeFontDescriptor(const char *path, const char *postscriptName, const char *family, const char *style,
+  FontWeight weight, FontWidth width, bool italic, bool monospace) {
+  if (path == NULL || postscriptName == NULL || family == NULL || style == NULL) {
+    return NULL;
+  }
+
+  return new FontDescriptor(
+    path,
+    postscriptName,
+    family,
+    style,
+    weight,
+    width,
+    italic,
+    monospace
+  );
+}
+
+long createFontDescriptor(FontDescriptor **res, CTFontDescriptorRef descriptor) {
   NSURL *url = (NSURL *) CTFontDescriptorCopyAttribute(descriptor, kCTFontURLAttribute);
-  NSString *psName = (NSString *) CTFontDescriptorCopyAttribute(descriptor, kCTFontNameAttribute);  
+  NSString *psName = (NSString *) CTFontDescriptorCopyAttribute(descriptor, kCTFontNameAttribute);
   NSString *family = (NSString *) CTFontDescriptorCopyAttribute(descriptor, kCTFontFamilyNameAttribute);
   NSString *style = (NSString *) CTFontDescriptorCopyAttribute(descriptor, kCTFontStyleNameAttribute);
-  
+
   NSDictionary *traits = (NSDictionary *) CTFontDescriptorCopyAttribute(descriptor, kCTFontTraitsAttribute);
   NSNumber *weightVal = traits[(id)kCTFontWeightTrait];
   FontWeight weight = (FontWeight) convertWeight([weightVal floatValue]);
-  
+
   NSNumber *widthVal = traits[(id)kCTFontWidthTrait];
   FontWidth width = (FontWidth) convertWidth([widthVal floatValue]);
-  
+
   NSNumber *symbolicTraitsVal = traits[(id)kCTFontSymbolicTrait];
   unsigned int symbolicTraits = [symbolicTraitsVal unsignedIntValue];
-    
-  FontDescriptor *res = new FontDescriptor(
+
+  *res = getMaybeFontDescriptor(
     [[url path] UTF8String],
     [psName UTF8String],
     [family UTF8String],
@@ -59,31 +77,35 @@ FontDescriptor *createFontDescriptor(CTFontDescriptorRef descriptor) {
     (symbolicTraits & kCTFontItalicTrait) != 0,
     (symbolicTraits & kCTFontMonoSpaceTrait) != 0
   );
-    
+
   [url release];
   [psName release];
   [family release];
   [style release];
   [traits release];
-  return res;
+  return *res == NULL ? -1 : 0;
 }
 
-ResultSet *getAvailableFonts() {
+long getAvailableFonts(ResultSet **resultSet) {
   // cache font collection for fast use in future calls
   static CTFontCollectionRef collection = NULL;
   if (collection == NULL)
     collection = CTFontCollectionCreateFromAvailableFonts(NULL);
-  
-  NSArray *matches = (NSArray *) CTFontCollectionCreateMatchingFontDescriptors(collection);  
-  ResultSet *results = new ResultSet();
-  
+
+  NSArray *matches = (NSArray *) CTFontCollectionCreateMatchingFontDescriptors(collection);
+
+  *resultSet = new ResultSet;
   for (id m in matches) {
     CTFontDescriptorRef match = (CTFontDescriptorRef) m;
-    results->push_back(createFontDescriptor(match));
+    FontDescriptor *fontDescriptor;
+    long error = createFontDescriptor(&fontDescriptor, match);
+    if (error == 0) {
+      (*resultSet)->push_back(fontDescriptor);
+    }
   }
-  
+
   [matches release];
-  return results;
+  return 0;
 }
 
 // helper to square a value
@@ -133,51 +155,55 @@ CTFontDescriptorRef getFontDescriptor(FontDescriptor *desc) {
   }
 
   // create a font descriptor and search for matches
-  return CTFontDescriptorCreateWithAttributes((CFDictionaryRef) attrs);  
+  return CTFontDescriptorCreateWithAttributes((CFDictionaryRef) attrs);
 }
 
 int metricForMatch(CTFontDescriptorRef match, FontDescriptor *desc) {
   NSDictionary *dict = (NSDictionary *)CTFontDescriptorCopyAttribute(match, kCTFontTraitsAttribute);
 
   bool italic = ([dict[(id)kCTFontSymbolicTrait] unsignedIntValue] & kCTFontItalicTrait);
-    
+
   // normalize everything to base-900
   int metric = 0;
   if (desc->weight)
     metric += sqr(convertWeight([dict[(id)kCTFontWeightTrait] floatValue]) - desc->weight);
-  
+
   if (desc->width)
     metric += sqr((convertWidth([dict[(id)kCTFontWidthTrait] floatValue]) - desc->width) * 100);
-  
+
   metric += sqr((italic != desc->italic) * 900);
 
   [dict release];
   return metric;
 }
 
-ResultSet *findFonts(FontDescriptor *desc) {
+long findFonts(ResultSet** fonts, FontDescriptor *desc) {
   CTFontDescriptorRef descriptor = getFontDescriptor(desc);
   NSArray *matches = (NSArray *) CTFontDescriptorCreateMatchingFontDescriptors(descriptor, NULL);
-  ResultSet *results = new ResultSet();
-  
+
   NSArray *sorted = [matches sortedArrayUsingComparator:^NSComparisonResult(id a, id b) {
     int ma = metricForMatch((CTFontDescriptorRef) a, desc);
     int mb = metricForMatch((CTFontDescriptorRef) b, desc);
     return ma < mb ? NSOrderedAscending : ma > mb ? NSOrderedDescending : NSOrderedSame;
   }];
-  
+
+  *fonts = new ResultSet;
   for (id m in sorted) {
     CTFontDescriptorRef match = (CTFontDescriptorRef) m;
     int mb = metricForMatch((CTFontDescriptorRef) m, desc);
-    
+
     if (mb < 10000) {
-      results->push_back(createFontDescriptor(match));
+      FontDescriptor *fontDescriptor;
+      long error = createFontDescriptor(&fontDescriptor, match);
+      if (error == 0) {
+        (*fonts)->push_back(fontDescriptor);
+      }
     }
   }
-  
+
   CFRelease(descriptor);
   [matches release];
-  return results;
+  return 0;
 }
 
 CTFontDescriptorRef findBest(FontDescriptor *desc, NSArray *matches) {
@@ -197,38 +223,36 @@ CTFontDescriptorRef findBest(FontDescriptor *desc, NSArray *matches) {
     if (metric == 0)
       break;
   }
-  
+
   return best;
 }
 
-FontDescriptor *findFont(FontDescriptor *desc) {  
-  FontDescriptor *res = NULL;
+long findFont(FontDescriptor **foundFont, FontDescriptor *desc) {
   CTFontDescriptorRef descriptor = getFontDescriptor(desc);
   NSArray *matches = (NSArray *) CTFontDescriptorCreateMatchingFontDescriptors(descriptor, NULL);
-  
+
   // if there was no match, try again but only try to match traits
   if ([matches count] == 0) {
     [matches release];
     NSSet *set = [NSSet setWithObjects:(id)kCTFontTraitsAttribute, nil];
     matches = (NSArray *) CTFontDescriptorCreateMatchingFontDescriptors(descriptor, (CFSetRef) set);
   }
-  
+
   // find the closest match for width and weight attributes
   CTFontDescriptorRef best = findBest(desc, matches);
-      
+
+  long error = -1;
   // if we found a match, generate and return a URL for it
-  if (best) {    
-    res = createFontDescriptor(best);
+  if (best) {
+    error = createFontDescriptor(foundFont, best);
   }
-  
+
   [matches release];
   CFRelease(descriptor);
-  return res;
+  return error;
 }
 
-FontDescriptor *substituteFont(char *postscriptName, char *string) {
-  FontDescriptor *res = NULL;
-  
+long substituteFont(FontDescriptor **res, char *postscriptName, char *string) {
   // create a font descriptor to find the font by its postscript name
   // we don't use CTFontCreateWithName because that supports font
   // names other than the postscript name but prints warnings.
@@ -236,18 +260,18 @@ FontDescriptor *substituteFont(char *postscriptName, char *string) {
   NSDictionary *attrs = @{(id)kCTFontNameAttribute: ps};
   CTFontDescriptorRef descriptor = CTFontDescriptorCreateWithAttributes((CFDictionaryRef) attrs);
   CTFontRef font = CTFontCreateWithFontDescriptor(descriptor, 12.0, NULL);
-  
+
   // find a substitute font that support the given characters
   NSString *str = [NSString stringWithUTF8String:string];
   CTFontRef substituteFont = CTFontCreateForString(font, (CFStringRef) str, CFRangeMake(0, [str length]));
   CTFontDescriptorRef substituteDescriptor = CTFontCopyFontDescriptor(substituteFont);
-  
+
   // finally, create and return a result object for this substitute font
-  res = createFontDescriptor(substituteDescriptor);
-  
+  long error = createFontDescriptor(res, substituteDescriptor);
+
   CFRelease(font);
   CFRelease(substituteFont);
   CFRelease(substituteDescriptor);
-  
-  return res;
+
+  return error;
 }
